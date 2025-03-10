ac5fb99f432292c856a105e8d2eb224f6cba5c5b
Abdullah Elamari <abdullah.elamari@gmail.com>
2025-03-10 08:17:21 -0400

    Task 2 - Your First Computer Program
    Task 2 Assignment

---
+++ hello_world.py
@@ -0,0 +1,15 @@
+# pseudocode section:
+# request the user to input their name.
+# create a variable to store the name
+# print out the name.
+# then ask the user to input their age.
+# create a variable to store the age
+# print out the age.
+# print “Hello, World!” on a new line.
+
+# python code section:
+name = input("What is your name?")
+print(name)
+age = input("How old are you?")
+print(age)
+print("Hello, World!")
