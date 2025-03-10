99eb31a16c31b205c9fee06263d4585b2eefa86e
Abdullah Elamari <abdullah.elamari@gmail.com>
2025-03-10 08:18:50 -0400

    Task 3 - Variables – Storing Data in Programs
    Task 3 Assignment

---
+++ details.py
@@ -0,0 +1,5 @@
+name = input ("Please provide your name")
+age = int(input("Please state how old you are")) # age will become an integer. 
+house_number = input ("Please provide your house number")
+street_name = input ("Please provide the name of your street")
+print("This is {}. They are {} years old and live at house number {} on {} Street.".format(name, age, house_number, street_name))
